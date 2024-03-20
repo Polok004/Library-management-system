3e3eb06b87b40676c3177da893c4340e33fdfc70
Polok004 <134692709+Polok004@users.noreply.github.com>
2024-03-20 20:13:02 +0600

    Update PL_SQL.sql

--- PL_SQL.sql
+++ PL_SQL.sql
@@ -1,1 +1,13 @@
+--insertion
+set serveroutput on
+declare 
+student_id Student.student_id%type:=9;
+student_name Student.student_name%type:='Akash Chowdhury';
+student_email Student.student_email%type:='akas@gmail.com';
+student_phone Student.student_phone%type:=0753579859;
+total_rented_books Student.total_rented_books%type:=0;
+begin
+insert into Student values(student_id,student_name,student_email,student_phone,total_rented_books);
+end;
 
+select * from student;
